32032d2f33c981b34b0bb1107a1aaa5ccaaaef19
Thomas Hochbichler <thomas.hochbichler.bp@nttdata.com>
2018-09-24 19:15:09 +0200

    add swagger

--- src/main/java/at/hochbichler/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/at/hochbichler/rest/webservices/restfulwebservices/user/User.java
@@ -1,14 +1,19 @@
 package at.hochbichler.rest.webservices.restfulwebservices.user;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
 
+@ApiModel(description = "User entity")
 public class User {
     private Integer id;
     @Size(min = 2, message = "Name should have at least 2 characters")
     private String name;
     @Past
+    @ApiModelProperty(notes = "Birthday should be in the past")
     private Date birthDate;
 
     protected User() {
