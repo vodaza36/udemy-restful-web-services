2032d730cc6324e163c6f97b938212bd975895c2
Thomas Hochbichler <thomas.hochbichler.bp@nttdata.com>
2018-09-24 20:46:29 +0200

    add filtering

---
+++ src/main/java/at/hochbichler/rest/webservices/restfulwebservices/filtering/FilteringController.java
@@ -0,0 +1,12 @@
+package at.hochbichler.rest.webservices.restfulwebservices.filtering;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FilteringController {
+    @GetMapping("/filtering")
+    public SomeBean retrieveSomeBean() {
+        return new SomeBean("value1", "value2", "value3");
+    }
+}
